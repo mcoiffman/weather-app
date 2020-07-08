794dc6365a8c243d16692779f62cbe054ef7fa10
Michael <mcoiffmansoftdev@gmail.com>
2020-07-08 02:37:41 -0400

    added icons and logic

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 window.addEventListener("load", () => {
   let long;
   let lat;
+  let temperatureDescription = document.querySelector(
+    ".temperature-description"
+  );
+  const temperatureDegree = document.querySelector(".temperature-degree");
+  const locationTimezone = document.querySelector(".location-timezone");
+  const temperatureSection = document.querySelector(".temperature");
+  const temperatureSpan = document.querySelector(".temperature span");
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -8,7 +15,6 @@ window.addEventListener("load", () => {
       lat = position.coords.latitude;
 
       const proxy = "https://cors-anywhere.herokuapp.com/";
-
       const api = `${proxy}http://api.weatherapi.com/v1/current.json?key=7b0a489f0cfa4be3bcb161628200707&q=${lat},${long}`;
 
       fetch(api)
@@ -16,8 +22,30 @@ window.addEventListener("load", () => {
           return apiResponse.json();
         })
         .then((data) => {
-            console.log(data);
+          const { temp_f } = data.current;
+          const { text, icon } = data.current.condition;
+          const { tz_id } = data.location;
+
+          temperatureDegree.textContent = temp_f;
+          temperatureDescription.textContent = text;
+          locationTimezone.textContent = tz_id;
+          setIconImageFromApi(icon);
+
+          temperatureSection.addEventListener("click", () => {
+            if (temperatureSpan.textContent === "F") {
+              temperatureSpan.textContent = "C";
+            } else {
+              temperatureSpan.textContent = "C";
+            }
+          });
         });
     });
+  }
+
+  function setIconImageFromApi(icon) {
+    const iconSplit = icon.split("64x64/");
+    document.getElementById(
+      "icon-image"
+    ).src = `icons/weather/64x64/${iconSplit[1]}`;
   }
 });
